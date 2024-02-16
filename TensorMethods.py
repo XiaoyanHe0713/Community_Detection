456b54a8bab8c5997b215231fc48620e4d658e11
Xiaoyan He <88536944+XiaoyanHe0713@users.noreply.github.com>
2024-02-16 01:18:56 -0800

    Update TensorMethods.py

--- TensorMethods.py
+++ TensorMethods.py
@@ -1,12 +1,27 @@
 import torch
 import numpy as np
+from tensorly.decomposition import tucker
+
+def unfold(tensor, mode):
+    return torch.reshape(torch.moveaxis(tensor, mode, 0), (tensor.shape[mode], -1))
+
+def fold(unfolded_tensor, mode, shape):
+    full_shape = list(shape)
+    mode_dim = full_shape.pop(mode)
+    full_shape.insert(0, mode_dim)
+    return torch.moveaxis(torch.reshape(unfolded_tensor, full_shape), 0, mode)
 
 def diag(A):
-    "A is a torch tensor, diagonal of A is the tensor whose (i1, . . . , im)"
-    "element is equal to the corresponding element of A if some indices of i_1, . . . , i_m are identical, and is equal to 0 otherwise"
-    "Our deﬁnition of diag is different from the conventional one. We use this definition for its convenience in dealing with self-edges."
-    "Returns a torch tensor with the diagonal of A"
-    
+    """ Diagonal of a tensor
+    Parameters
+    ----------
+    A: is a torch tensor, diagonal of A is the tensor whose (i1, . . . , im) element is equal to the corresponding element of A if some 
+    indices of i_1, . . . , i_m are identical, and is equal to 0 otherwise.
+    Our deﬁnition of diag is different from the conventional one. We use this definition for its convenience in dealing with self-edges.
+    Returns
+    -------
+    diag_A: torch tensor of the same shape as A containing the diagonal of A
+    """
     # Get the shape of the tensor
     shape = A.shape
     
@@ -21,4 +36,97 @@ def diag(A):
             diag_A[index] = 0
     
     return diag_A
+        
+def multi_mode_dot(tensor, matrix_or_vec_list, modes=None, skip=None, transpose=False):
+    """Chain several mode_dot (n-mode product) in one go"""
+    if modes is None:
+        modes = range(len(matrix_or_vec_list))
 
+    decrement = 0  # If we multiply by a vector, we diminish the dimension of the tensor
+
+    res = tensor
+
+    # Order of mode dots doesn't matter for different modes
+    # Sorting by mode shouldn't change order for equal modes
+    factors_modes = sorted(zip(matrix_or_vec_list, modes), key=lambda x: x[1])
+    for i, (matrix_or_vec, mode) in enumerate(factors_modes):
+        if (skip is not None) and (i == skip):
+            continue
+
+        if transpose:
+            res = mode_dot(res, torch.conj(torch.transpose(matrix_or_vec)), mode - decrement)
+        else:
+            res = mode_dot(res, matrix_or_vec, mode - decrement)
+
+        if torch.ndim(matrix_or_vec) == 1:
+            decrement += 1
+
+    return res
+
+def mode_dot(tensor, matrix_or_vector, mode, transpose=False):
+    """n-mode product of a tensor and a matrix or vector at the specified mode
+
+    Mathematically: :math:`\\text{tensor} \\times_{\\text{mode}} \\text{matrix or vector}`
+
+    Parameters
+    ----------
+    tensor : ndarray
+        tensor of shape ``(i_1, ..., i_k, ..., i_N)``
+    matrix_or_vector : ndarray
+        1D or 2D array of shape ``(J, i_k)`` or ``(i_k, )``
+        matrix or vectors to which to n-mode multiply the tensor
+    mode : int
+    transpose : bool, default is False
+        If True, the matrix is transposed.
+        For complex tensors, the conjugate transpose is used.
+
+    Returns
+    -------
+    ndarray
+        `mode`-mode product of `tensor` by `matrix_or_vector`
+        * of shape :math:`(i_1, ..., i_{k-1}, J, i_{k+1}, ..., i_N)` if matrix_or_vector is a matrix
+        * of shape :math:`(i_1, ..., i_{k-1}, i_{k+1}, ..., i_N)` if matrix_or_vector is a vector
+    """
+    # the mode along which to fold might decrease if we take product with a vector
+    fold_mode = mode
+    new_shape = list(tensor.shape)
+
+    if torch.ndim(matrix_or_vector) == 2:  # Tensor times matrix
+        # Test for the validity of the operation
+        dim = 0 if transpose else 1
+        if matrix_or_vector.shape[dim] != tensor.shape[mode]:
+            raise ValueError(
+                f"shapes {tensor.shape} and {matrix_or_vector.shape} not aligned in mode-{mode} multiplication: "
+                f"{tensor.shape[mode]} (mode {mode}) != {matrix_or_vector.shape[dim]} (dim 1 of matrix)"
+            )
+
+        if transpose:
+            matrix_or_vector = torch.conj(torch.transpose(matrix_or_vector))
+
+        new_shape[mode] = matrix_or_vector.shape[0]
+        vec = False
+
+    elif torch.ndim(matrix_or_vector) == 1:  # Tensor times vector
+        if matrix_or_vector.shape[0] != tensor.shape[mode]:
+            raise ValueError(
+                f"shapes {tensor.shape} and {matrix_or_vector.shape} not aligned for mode-{mode} multiplication: "
+                f"{tensor.shape[mode]} (mode {mode}) != {matrix_or_vector.shape[0]} (vector size)"
+            )
+        if len(new_shape) > 1:
+            new_shape.pop(mode)
+        else:
+            new_shape = ()
+        vec = True
+
+    else:
+        raise ValueError(
+            "Can only take n_mode_product with a vector or a matrix."
+            f"Provided array of dimension {torch.ndim(matrix_or_vector)} not in [1, 2]."
+        )
+
+    res = torch.dot(matrix_or_vector, unfold(tensor, mode))
+
+    if vec:  # We contracted with a vector, leading to a vector
+        return torch.reshape(res, shape=new_shape)
+    else:  # tensor times vec: refold the unfolding
+        return fold(res, fold_mode, new_shape)
